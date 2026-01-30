3aaf4e5720040d6871bef879aad87f48993b9792
gaelgerard <gael.gerard@free.fr>
2026-01-30 16:18:27 +0100

    feat: custom excerpt for permalink outside <p

--- web/app/themes/w3-sage/app/filters.php
+++ web/app/themes/w3-sage/app/filters.php
@@ -11,6 +11,16 @@ namespace App;
  *
  * @return string
  */
-add_filter('excerpt_more', function () {
-    return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
-});
+// excerpt_more should be set the empty.
+add_filter( 'excerpt_more', '__return_empty_string', 21 );
+
+function wpse_134143_excerpt_more_link( $excerpt ) {
+    $excerpt .= sprintf('<a class="group no-underline mt-4 inline-flex items-center text-sm text-gray-600 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100" href="%s">%s <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right ml-1 h-4 w-4 transition-transform group-hover:translate-x-0.5"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg></a>', 
+    get_permalink(), 
+    __('Read article', 'sage')
+    );
+
+    return $excerpt;
+}
+// Utilisez la constante magique __NAMESPACE__ pour plus de sécurité
+add_filter( 'the_excerpt', __NAMESPACE__ . '\\wpse_134143_excerpt_more_link', 21 );
