13bcbfacb847bf26a83b535cf129b53f1e9676a8
gaelgerard <gael.gerard@free.fr>
2026-02-02 11:31:29 +0100

    feat: styling blog

--- web/app/themes/w3-sage/app/filters.php
+++ web/app/themes/w3-sage/app/filters.php
@@ -15,7 +15,7 @@ namespace App;
 add_filter( 'excerpt_more', '__return_empty_string', 21 );
 
 function wpse_134143_excerpt_more_link( $excerpt ) {
-    $excerpt .= sprintf('<a class="group no-underline mt-4 inline-flex items-center text-sm text-gray-600 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100" href="%s">%s <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right ml-1 h-4 w-4 transition-transform group-hover:translate-x-0.5"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg></a>', 
+    $excerpt .= sprintf('<a class="group no-underline mt-4 inline-flex items-center text-sm text-gray-600  hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100" href="%s">%s <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right ml-1 h-4 w-4 transition-transform group-hover:translate-x-0.5"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg></a>', 
     get_permalink(), 
     __('Read article', 'sage')
     );
@@ -179,4 +179,19 @@ function sage_comment_callback($comment, $args, $depth)
 add_filter('wp_list_comments_args', function ($args) {
     $args['callback'] = __NAMESPACE__ . '\\sage_comment_callback';
     return $args;
+});
+
+add_filter('get_the_archive_title', function ($title) {
+    if (is_category()) {
+        $title = single_cat_title('', false);
+    } elseif (is_tag()) {
+        $title = single_tag_title('', false);
+    } elseif (is_author()) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif (is_tax()) { //for custom post types
+        $title = sprintf(__('%1$s'), single_term_title('', false));
+    } elseif (is_post_type_archive()) {
+        $title = post_type_archive_title('', false);
+    }
+    return $title;
 });
--- web/app/themes/w3-sage/resources/views/partials/page-header.blade.php
+++ web/app/themes/w3-sage/resources/views/partials/page-header.blade.php
@@ -1,3 +1,3 @@
-<div class="page-header">
-  <h1>{!! $title !!}</h1>
+<div class="page-header mb-6">
+  <h1 class="text-3xl font-bold text-gray-900 sm:text-4xl dark:text-gray-100">{!! $title !!}</h1>
 </div>
