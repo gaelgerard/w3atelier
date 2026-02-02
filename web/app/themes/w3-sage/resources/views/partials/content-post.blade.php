919064b7304dffaa7c7e42b937f058821c006e31
gaelgerard <gael.gerard@free.fr>
2026-02-02 11:38:01 +0100

    feat: template for displaying posts in loops

---
+++ web/app/themes/w3-sage/resources/views/partials/content-post.blade.php
@@ -0,0 +1,33 @@
+<li class="py-12 first:pt-0">
+    <article>
+        <div class="space-y-8">
+        <div class="space-y-4">
+            <div class="flex flex-col gap-4 text-sm text-gray-500 dark:text-gray-400">
+            <dl>
+                <dt class="sr-only">Published on</dt>
+                <dd class="text-base font-medium leading-6 text-gray-500 dark:text-gray-200">
+                <time datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+                </dd>
+            </dl>
+            <h3 class="text-2xl font-semibold text-gray-900 dark:text-gray-100">
+                <a href="{{ get_permalink() }}" class="break-words no-underline">
+                    {!! get_the_title() !!}
+                </a>
+                
+            </h3>
+                @php($tags = get_the_tags())
+                @if($tags)
+                <div class="flex flex-wrap gap-2">
+                @foreach($tags as $tag)
+                    <a class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase no-underline" href="{{ get_tag_link($tag->term_id) }}">
+                    {{ $tag->name }}
+                    </a>
+                @endforeach
+                </div>
+                @endif
+            </div>
+            @php(the_excerpt())
+
+        </div>
+    </article>
+</li>
