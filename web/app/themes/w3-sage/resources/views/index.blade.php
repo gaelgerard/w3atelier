d46ea094b7f3d69c0d2622b6fc4c294623448c54
gaelgerard <gael.gerard@free.fr>
2026-02-02 17:14:50 +0100

    feat: archive page with dynamic sidebar

--- web/app/themes/w3-sage/resources/views/index.blade.php
+++ web/app/themes/w3-sage/resources/views/index.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
   @include('partials.page-header')
-
-  @if (! have_posts())
-    <x-alert type="warning">
-      {!! __('Sorry, no results were found.', 'sage') !!}
-    </x-alert>
-
-    {!! get_search_form(false) !!}
+  @if ( is_archive() )
+    @if ( category_description() )
+    <div class="prose space-y-2">
+      {!! category_description() !!}
+    </div>
+    @endif
   @endif
+  <div class="flex flex-col gap-8 md:flex-row">
+    @section('sidebar')
+    @endsection
+    @include('sections.archive-sidebar')
+    
+    <div class="min-w-0 flex-1">
+      @if (! have_posts())
+        <x-alert type="warning">
+          {!! __('Sorry, no results were found.', 'sage') !!}
+        </x-alert>
 
-  @while(have_posts()) @php(the_post())
-    @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
-  @endwhile
+        {!! get_search_form(false) !!}
+      @endif
+    <ul class="divide-y divide-gray-200 dark:divide-gray-800">
+      @while(have_posts()) @php(the_post())
+        @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
+      @endwhile
+    </ul>
+    
+      {!! get_the_posts_navigation() !!}
+    @endsection
+    </div>
 
-  {!! get_the_posts_navigation() !!}
-@endsection
-
-@section('sidebar')
-  @include('sections.sidebar')
-@endsection
+  </div>
---
+++ web/app/themes/w3-sage/resources/views/sections/archive-sidebar.blade.php
@@ -0,0 +1,24 @@
+<aside class="hidden md:block md:w-64">
+    <div class="sticky top-24 space-y-8">
+        <div class="space-y-4">
+            <h2 class="font-mono text-sm tracking-wider text-gray-500 uppercase dark:text-gray-300">{!! __('Filter by tag', 'sage') !!}</h2>
+            <nav class="flex flex-col space-y-3">
+                {{ App\custom_tag_list() }}
+            </nav>
+            <h2 class="font-mono text-sm tracking-wider text-gray-500 uppercase dark:text-gray-300">{!! __('Filter by category', 'sage') !!}</h2>
+            <nav class="flex flex-col space-y-3">
+                <ul>
+                    {{ 
+                        wp_list_categories(
+                        array(
+                        'title_li' => '',
+                        'hide_title_if_empty' => true,
+                        'show_count' => 1,                        
+                        'walker' => new App\Custom_Walker_Category(),
+                        ) );
+                    }}
+                </ul>
+            </nav>
+        </div>
+    </div>
+</aside>
