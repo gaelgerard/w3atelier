87b99213983a3256c43a3935b0217cec55f3c00d
gaelgerard <gael.gerard@free.fr>
2026-02-05 11:40:56 +0100

    feat: checkmark on copied code block in js instead of css

--- web/app/themes/w3-sage/resources/js/app.js
+++ web/app/themes/w3-sage/resources/js/app.js
@@ -35,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // 2. On crée le bouton
     const button = document.createElement('button');
     const btnIcon = '<svg aria-hidden="true" class="svg-icon iconCopy mr4" width="17" height="18" viewBox="0 0 17 18"><path d="M5 6c0-1.09.91-2 2-2h4.5L15 7.5V15c0 1.09-.91 2-2 2H7c-1.09 0-2-.91-2-2zm6-1.25V8h3.25z"></path><path d="M10 1a2 2 0 0 1 2 2H6a2 2 0 0 0-2 2v9a2 2 0 0 1-2-2V4a3 3 0 0 1 3-3z" opacity=".4"></path></svg>';
+    const checkMark = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>';
     button.innerHTML = btnIcon + ' Copier';
     button.className = 'copy-code-button'; // Pour le styliser en CSS
 
@@ -48,11 +49,11 @@ document.addEventListener('DOMContentLoaded', () => {
       
       navigator.clipboard.writeText(code).then(() => {
         // Petit feedback visuel
-        button.innerText = 'Copié !';
+        button.innerHTML = checkMark + 'Copié !';
         button.classList.add('copied');
 
         setTimeout(() => {
-          button.innerText = 'Copier';
+          button.innerHTML = btnIcon + 'Copier';
           button.classList.remove('copied');
         }, 2000);
       }).catch(err => {
