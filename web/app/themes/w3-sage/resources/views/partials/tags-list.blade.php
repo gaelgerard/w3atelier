fe7da27d161bad672375da363d87db55c94a15c0
gaelgerard <gael.gerard@free.fr>
2026-02-06 19:18:23 +0100

    feat: different display for tag list cloud

--- web/app/themes/w3-sage/resources/views/partials/tags-list.blade.php
+++ web/app/themes/w3-sage/resources/views/partials/tags-list.blade.php
@@ -1,12 +1,30 @@
-<ul class="{{ $isCloud ? 'flex flex-wrap gap-2' : 'divide-y divide-gray-100' }}">
+@if ($isCloud)
+    <div class="flex max-w-lg flex-wrap">
     @foreach ($tags as $tag)
-        <li>
+            <div class="mt-2 mr-5 mb-2">
             <a href="{{ esc_url(get_tag_link($tag->term_id)) }}"
                title="{{ esc_attr(sprintf(__('View all posts tagged under %s'), $tag->name)) }}"
-               class="{{ implode(' ', $classes($tag->term_id, $current_tag_id)) }}">
-                {{ esc_html($tag->name) }} 
-                <span class="opacity-50 text-[10px]">({{ intval($tag->count) }})</span>
+               class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase mr-0 {{ implode(' ', $classes($tag->term_id, $current_tag_id)) }}">
+                {{ esc_html($tag->name) }}
             </a>
-        </li>
+            <a href="{{ esc_url(get_tag_link($tag->term_id)) }}"
+               title="{{ esc_attr(sprintf(__('View all posts tagged under %s'), $tag->name)) }}"
+               class="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300 {{ implode(' ', $classes($tag->term_id, $current_tag_id)) }}">
+                ({{ intval($tag->count) }})
+            </a>
+            </div>
     @endforeach
+    </div>
+@else
+    <ul>
+        @foreach ($tags as $tag)
+            <li>
+                <a href="{{ esc_url(get_tag_link($tag->term_id)) }}"
+                   title="{{ esc_attr(sprintf(__('View all posts tagged under %s'), $tag->name)) }}"
+                   class="{{ implode(' ', $classes($tag->term_id, $current_tag_id)) }}">
+                    {{ esc_html($tag->name) }} ({{ intval($tag->count) }})
+                </a>
+            </li>
+        @endforeach
 </ul>
+@endif
