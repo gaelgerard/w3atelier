1be389b4d429bb298234ddc631c355113cbd2893
gaelgerard <gael.gerard@free.fr>
2026-02-09 16:28:33 +0100

    feat: define debug in env file

--- config/application.php
+++ config/application.php
@@ -137,8 +137,9 @@ Config::define('CONCATENATE_SCRIPTS', false);
 /**
  * Debugging Settings
  */
-Config::define('WP_DEBUG_DISPLAY', false);
-Config::define('WP_DEBUG_LOG', false);
+Config::define('WP_DEBUG', env('WP_DEBUG') ?: false);
+Config::define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY') ?: false);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?: false);
 Config::define('SCRIPT_DEBUG', false);
 ini_set('display_errors', '0');
 
