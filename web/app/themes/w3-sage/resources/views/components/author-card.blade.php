a8765d1da85197cb161e0a7fb9a31b3ee431e8ff
gaelgerard <gael.gerard@free.fr>
2026-02-10 17:38:43 +0100

    feat: add SO link to authorcard

--- web/app/themes/w3-sage/resources/views/components/author-card.blade.php
+++ web/app/themes/w3-sage/resources/views/components/author-card.blade.php
@@ -25,7 +25,7 @@
 
     <div class="flex gap-4">
         {{-- Lien Mail --}}
-        <a class="group text-gray-700 dark:text-gray-200 transition-colors" href="/contact" title="Me contacter">
+        <a class="group text-gray-700 dark:text-gray-200 transition-colors" href="/accompagnement" title="Me contacter">
             <span class="sr-only">Email</span>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="h-6 w-6 fill-current group-hover:text-primary-500 transition-colors">
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
@@ -40,7 +40,24 @@
                 <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
             </svg>
         </a>
+        {{-- Lien StackOverflow --}}
 
+        <a
+            class="group text-gray-700 dark:text-gray-200 transition-colors"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://stackoverflow.com/users/3623080/gael"
+            title="Mon profil StackOverflow"
+        >
+            <span class="sr-only">Stack Overflow</span>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                class="h-6 w-6 fill-current group-hover:text-primary-500 transition-colors"
+            >
+                <path d="M18.986 21.865v-6.404h2.134V24H1.844v-8.539h2.13v6.404h15.012zM6.111 19.731H16.85v-2.137H6.111v2.137zm.259-4.852l10.48 2.189.437-2.093-10.481-2.19-.436 2.094zm1.504-4.506l9.341 4.54.912-1.928-9.34-4.54-.913 1.928zm3.263-4.045l7.152 7.37 1.517-1.472-7.152-7.37-1.517 1.472zm5.342-3.153l-4.708 9.077 1.884.978 4.708-9.077-1.884-.978z"/>
+            </svg>
+        </a>
         {{-- Lien LinkedIn --}}
         <a class="group text-gray-700 dark:text-gray-200 transition-colors" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/gael-gerard" title="Mon profil LinkedIn">
             <span class="sr-only">LinkedIn</span>
