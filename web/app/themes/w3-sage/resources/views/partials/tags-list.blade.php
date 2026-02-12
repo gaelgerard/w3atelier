ab3adc19e5caedbbd1167b03854d6325e3b1c9df
gaelgerard <gael.gerard@free.fr>
2026-02-12 16:43:08 +0100

    feat: colors tags list and width

--- web/app/themes/w3-sage/resources/views/partials/tags-list.blade.php
+++ web/app/themes/w3-sage/resources/views/partials/tags-list.blade.php
@@ -1,15 +1,16 @@
 @if ($isCloud)
-    <div class="flex max-w-xl flex-wrap">
+    <div class="flex max-w-5xl flex-wrap">
     @foreach ($tags as $tag)
-            <div class="mt-2 mr-5 mb-2">
+            <div class="mt-2 mr-5 mb-2 group">
             <a href="{{ esc_url(get_tag_link($tag->term_id)) }}"
                title="{{ esc_attr(sprintf(__('View all posts tagged under %s'), $tag->name)) }}"
-               class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase mr-0 {{ implode(' ', $classes($tag->term_id, $current_tag_id)) }}">
-                {{ esc_html($tag->name) }}
+               class="text-primary-500 hover:text-blue-700 mr-2 
+               text-sm font-medium uppercase mr-0 no-underline group-transition-color">
+                {!! esc_html($tag->name) !!}
             </a>
             <a href="{{ esc_url(get_tag_link($tag->term_id)) }}"
                title="{{ esc_attr(sprintf(__('View all posts tagged under %s'), $tag->name)) }}"
-               class="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300 {{ implode(' ', $classes($tag->term_id, $current_tag_id)) }}">
+               class="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300 group-transition-color {{ implode(' ', $classes($tag->term_id, $current_tag_id)) }}">
                 ({{ intval($tag->count) }})
             </a>
             </div>
@@ -22,7 +23,7 @@
                 <a href="{{ esc_url(get_tag_link($tag->term_id)) }}"
                    title="{{ esc_attr(sprintf(__('View all posts tagged under %s'), $tag->name)) }}"
                    class="{{ implode(' ', $classes($tag->term_id, $current_tag_id)) }}">
-                    {{ esc_html($tag->name) }} ({{ intval($tag->count) }})
+                    {!! esc_html($tag->name) !!} ({{ intval($tag->count) }})
                 </a>
             </li>
         @endforeach
