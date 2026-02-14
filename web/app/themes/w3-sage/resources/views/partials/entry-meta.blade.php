61152b728cb2eead3836125f37fa2cc076a8d8ad
gaelgerard <gael.gerard@free.fr>
2026-02-14 19:07:09 +0100

    feat: isSignificantlyModified on single post entry meta

--- web/app/themes/w3-sage/resources/views/partials/entry-meta.blade.php
+++ web/app/themes/w3-sage/resources/views/partials/entry-meta.blade.php
@@ -1,4 +1,9 @@
 <time class="dt-published" datetime="{{ get_post_time('c', true) }}">
   {{ get_the_date() }}
 </time>
+@if($is_significantly_modified)
+<time class="updated" datetime="{{ get_the_modified_time('c') }}">
+    ({{__('Updated on:', 'sage')}} {{ get_the_modified_date() }})
+</time>
+@endif
 
