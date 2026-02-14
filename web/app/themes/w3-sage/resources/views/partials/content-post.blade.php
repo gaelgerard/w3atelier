443dacdbda12a33ad4ff1ac661059c053599ea58
gaelgerard <gael.gerard@free.fr>
2026-02-14 18:53:44 +0100

    feat: modified date on posts if modified 1 month +

--- web/app/themes/w3-sage/resources/views/partials/content-post.blade.php
+++ web/app/themes/w3-sage/resources/views/partials/content-post.blade.php
@@ -1,12 +1,22 @@
+@php
+  $u_time = get_the_time('U'); 
+  $u_modified_time = get_the_modified_time('U'); 
+  $un_mois_en_secondes = 30 * 24 * 60 * 60;
+@endphp
 <li class="py-12 first:pt-0">
     <article>
         <div class="space-y-8">
         <div class="space-y-4">
             <div class="flex flex-col gap-4 text-sm text-gray-500 dark:text-gray-400">
             <dl>
-                <dt class="sr-only">Published on</dt>
+                <dt class="sr-only">{__('Published on', 'sage')}</dt>
                 <dd class="text-base font-medium leading-6 text-gray-500 dark:text-gray-200">
                 <time datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+                @if ($u_modified_time >= ($u_time + $un_mois_en_secondes))
+                <time class="updated" datetime="{{ get_the_modified_time('c') }}">
+                    ({{__('Updated on:', 'sage')}} {{ get_the_modified_date() }})
+                </time>
+                @endif
                 </dd>
             </dl>
             <h3 class="text-2xl font-semibold text-gray-900 dark:text-gray-100">
