c31142d869344cd15716602e6f8466f951a89285
gaelgerard <gael.gerard@free.fr>
2026-02-16 12:11:56 +0100

    feat: colors

--- web/app/themes/w3-sage/resources/views/sections/header.blade.php
+++ web/app/themes/w3-sage/resources/views/sections/header.blade.php
@@ -1,10 +1,10 @@
 <header class="banner flex py-10">
   @if (has_custom_logo())
   {!! get_custom_logo() !!} 
-  <a class="brand text-4xl md:text-3xl ml-2 font-bold font-sans text-gray-900 dark:text-white no-underline mb-2" href="{{ home_url('/') }}">
+  <a class="brand text-4xl md:text-3xl ml-2 font-bold font-sans no-underline mb-2" href="{{ home_url('/') }}">
   Blog
   @else
-  <a class="brand text-2xl font-bold font-sans text-gray-900 dark:text-white no-underline" href="{{ home_url('/') }}">
+  <a class="brand text-2xl font-bold font-sans no-underline" href="{{ home_url('/') }}">
         {!! $site_name !!}
     @endif
   </a>
