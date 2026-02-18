5ba58df34b55c66c68193a99c302d57a93c3cc57
gaelgerard <gael.gerard@free.fr>
2026-02-18 16:45:34 +0100

    feat: critical css plugin for LCP

--- web/app/themes/w3-sage/vite.config.js
+++ web/app/themes/w3-sage/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin'
 import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
+import critical from 'rollup-plugin-critical';
+// Déterminez l'URL selon l'environnement
+const siteUrl = process.env.NODE_ENV === 'production' 
+    ? 'https://blog.gaelgerard.com' 
+    : 'http://w3.gg';
 
 export default defineConfig({
   base: '/app/themes/w3-sage/public/build/',
@@ -25,6 +30,19 @@ export default defineConfig({
       disableTailwindColors: false,
       disableTailwindFonts: false,
       disableTailwindFontSizes: false,
+    }),
+    // Ajout du plugin Critical
+    critical({
+      criticalUrl: siteUrl, // L'URL de votre blog Bedrock
+      criticalBase: 'public/build',
+      criticalPages: [
+        { uri: '/', template: 'index' }, // La home
+        { uri: '/wordpress-cms/creation-de-plateforme-de-cours-en-ligne-wordpress-retour-dexperience-et-choix-techniques', template: 'single' }, // Un article pour le LCP
+      ],
+      criticalConfig: {
+        width: 1300,
+        height: 900,
+      },
     }),
   ],
   resolve: {
