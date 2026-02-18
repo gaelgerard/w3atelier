737e7ad35377b2f5d98a48eced97aa2b4d827cab
gaelgerard <gael.gerard@free.fr>
2026-02-18 15:01:20 +0100

    feat: footer base line multi line mobile

--- web/app/themes/w3-sage/resources/views/sections/footer.blade.php
+++ web/app/themes/w3-sage/resources/views/sections/footer.blade.php
@@ -73,16 +73,18 @@
         </svg>
       </a>
     </div>
-    <div class="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+    <div class="mb-2 flex flex-wrap items-center justify-center sm:justify-start gap-x-2 gap-y-1 text-sm text-gray-500 dark:text-gray-400 text-center sm:text-left">
       <div>Gaël GÉRARD</div>
       <div> • </div>
-      <div>©  {{ date('Y') }} </div>
-      <div> • </div>
-      <a class="break-words" href="https://www.gaelgerard.com" target="_blank">gaelgerard.com</a>
-                <p class="flex items-center gap-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
-                    <a href="tel:+33285523866" class="hover:text-primary-500 transition-colors no-underline">+33 (0)2 85 52 38 66</a>
-                </p>
+      <div>© {{ date('Y') }} </div>
+      <div class="hidden sm:block"> • </div> 
+      
+      <a class="break-words w-full hover:text-blue-500 sm:w-auto" href="https://www.gaelgerard.com" target="_blank">gaelgerard.com</a>
+      
+      <p class="flex items-center justify-center gap-1 w-full sm:w-auto">
+          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
+          <a href="tel:+33285523866" class="hover:text-blue-500 transition-colors no-underline">+33 (0)2 85 52 38 66</a>
+      </p>
     </div>
     <div class="mb-8 text-sm text-gray-500 dark:text-gray-400">
       <a class="break-words" target="_blank" rel="noopener noreferrer" href="https://github.com/gaelgerard/w3atelier">Tailwind WordPress Theme</a>
