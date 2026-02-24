553f86395269d6344618d3a6e6a8044c56be9d63
gaelgerard <gael.gerard@free.fr>
2026-02-24 15:16:28 +0100

    fix: add default db values to prevent local error establishing connection

--- config/application.php
+++ config/application.php
@@ -87,9 +87,9 @@ if (env('DB_SSL')) {
     Config::define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
 }
 
-Config::define('DB_NAME', env('DB_NAME'));
-Config::define('DB_USER', env('DB_USER'));
-Config::define('DB_PASSWORD', env('DB_PASSWORD'));
+Config::define('DB_NAME', env('DB_NAME') ?: 'gaelgerard_w3ggcom');
+Config::define('DB_USER', env('DB_USER') ?: 'root');
+Config::define('DB_PASSWORD', env('DB_PASSWORD') ?: 'root');
 Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
 Config::define('DB_CHARSET', 'utf8mb4');
 Config::define('DB_COLLATE', '');
